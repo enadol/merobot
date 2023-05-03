897f387822bab57fb19e50d7e85716397336171d
enadol <enadol@yahoo.com>
2023-05-03 20:39:49 +0200

    modified:   1-fc-koeln.json 	modified:   1-fc-koeln.txt 	modified:   fc-augsburg.json 	modified:   fc-augsburg.txt 	modified:   hertha-bsc.json 	modified:   hertha-bsc.txt 	modified:   scrpapteamwlptr.py 	modified:   vfb-stuttgart.json 	modified:   vfb-stuttgart.txt 	modified:   vfl-bochum.json 	modified:   vfl-bochum.txt 	modified:   vfl-wolfsburg.json 	modified:   vfl-wolfsburg.txt

--- scrpapteamwlptr.py
+++ scrpapteamwlptr.py
@@ -26,7 +26,7 @@ revert=["Dikeni Salifou"]
 vereinslos=["Max Kruse"]
 
 #Bor Mönchengladbach para Gladbach
-club="Eintracht Frankfurt"
+club="Hertha BSC"
 torneo="2022-23"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
