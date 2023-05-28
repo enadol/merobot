0cabb77847c6d200530544869fac7078e10dc8fd
enadol <enadol@yahoo.com>
2023-05-28 20:06:43 +0200

    modified:   scrpapteamwlptr.py 	modified:   tsg-hoffenheim.json 	modified:   tsg-hoffenheim.txt 	modified:   vfl-bochum.json 	modified:   vfl-bochum.txt 	modified:   vfl-wolfsburg.json 	modified:   vfl-wolfsburg.txt

--- scrpapteamwlptr.py
+++ scrpapteamwlptr.py
@@ -26,7 +26,7 @@ revert=["Dikeni Salifou"]
 vereinslos=["Max Kruse"]
 
 #Bor Mönchengladbach para Gladbach
-club="Bayer 04 Leverkusen"
+club="TSG Hoffenheim"
 torneo="2022-23"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
