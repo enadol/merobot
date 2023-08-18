9b41e636cdb4c9827527adf90f88739309d948ee
enadol <enadol@yahoo.com>
2023-08-18 11:56:09 +0200

    modified:   1-fc-koeln.json 	modified:   1-fc-koeln.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -29,7 +29,7 @@ vereinslos=["Max Kruse"]
 #prodigos=["Alexander Nübel", "Malik Tillman"]
 
 #Bor Mönchengladbach para Gladbach
-club="RB Leipzig"
+club="1 FC Köln"
 torneo="2023-24"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
