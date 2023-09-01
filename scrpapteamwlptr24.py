53c14e1339918ed5b8d157bb7a709c43712d0ede
enadol <enadol@yahoo.com>
2023-09-01 13:48:34 +0200

    modified:   bayern-muenchen.json 	modified:   bayern-muenchen.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -30,7 +30,7 @@ vereinslos=["Max Kruse"]
 
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen
-club="Bor Mönchengladbach"
+club="Bayern München"
 torneo="2023-24"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
