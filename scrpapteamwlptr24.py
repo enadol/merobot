4db111e0572fe030db246188b73f97edc999f96a
enadol <enadol@yahoo.com>
2023-09-10 18:46:22 +0200

    modified:   1-fc-heidenheim.json 	modified:   1-fc-heidenheim.txt 	modified:   1-fsv-mainz-05.json 	modified:   1-fsv-mainz-05.txt 	modified:   scrpapteamwlptr24.py 	modified:   sv-darmstadt-98.json 	modified:   sv-darmstadt-98.txt 	modified:   tsg-hoffenheim.json 	modified:   tsg-hoffenheim.txt 	modified:   werder-bremen.json 	modified:   werder-bremen.txt

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -29,8 +29,8 @@ vereinslos=["Max Kruse"]
 #prodigos=["Alexander Nübel", "Malik Tillman"]
 
 #Bor Mönchengladbach para Gladbach
-#Bayer 04 Leverkusen
-club="VfL Bochum"
+#Bayer 04 Leverkusen 1FC Heidenheim
+club="1 FC Heidenheim"
 torneo="2023-24"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
