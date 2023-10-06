e77f8d5c9d9c839b4eb4d0fa4ba042a2ecfd60e3
enadol <enadol@yahoo.com>
2023-10-06 13:42:16 +0200

    modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -30,7 +30,7 @@ vereinslos=["Max Kruse"]
 
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1FC Heidenheim
-club="Werder Bremen"
+club="TSG Hoffenheim"
 torneo="2023-24"
 klassvita=["kick__vita__header__person-detail-kvpair-info"]
 klassfrom=["kick__vita__header__team-detail__prime"]
