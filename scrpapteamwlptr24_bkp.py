253c382e0aac5fafa1bf481143592291dc67ece2
enadol <enadol@yahoo.com>
2024-02-24 07:24:44 +0100

    modified:   bayer-04-leverkusen.json 	modified:   bayer-04-leverkusen.txt 	modified:   scrpapteamwlptr24_bkp.py

--- scrpapteamwlptr24_bkp.py
+++ scrpapteamwlptr24_bkp.py
@@ -31,7 +31,7 @@ vereinslos=["Max Kruse"]
 
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1FC Heidenheim
-club="FC Bayern München"
+club="Bayer 04 Leverkusen"
 torneo="2023-24"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
