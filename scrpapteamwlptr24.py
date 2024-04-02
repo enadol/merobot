6dbf99114f6747f9f13d72665ca1e595fcfd415b
enadol <enadol@yahoo.com>
2024-04-02 20:37:12 +0200

    modified:   borussia-dortmund.json 	modified:   borussia-dortmund.txt 	modified:   fc-bayern-muenchen.json 	modified:   fc-bayern-muenchen.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -35,7 +35,7 @@ vereinslos=["Max Kruse", "Anwar El Ghazi"]
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1FC Heidenheim
 
-club="RB Leipzig"
+club="FC Bayern München"
 torneo="2023-24"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
