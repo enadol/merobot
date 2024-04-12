e33bf318961671c6543c3740eb0b105c6652ee51
enadol <enadol@yahoo.com>
2024-04-12 10:08:06 +0200

    modified:   eintracht-frankfurt.json 	modified:   eintracht-frankfurt.txt 	modified:   scrpapteamwlptr24.py 	modified:   vfb-stuttgart.json 	modified:   vfb-stuttgart.txt

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -35,7 +35,7 @@ vereinslos=["Max Kruse", "Anwar El Ghazi"]
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1FC Heidenheim
 
-club="RB Leipzig"
+club="Eintracht Frankfurt"
 torneo="2023-24"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
