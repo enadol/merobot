c0859d700fc3a0470b015c6611c4cd0bb6202327
enadol <enadol@yahoo.com>
2024-07-29 10:41:24 +0200

    modified:   1-fc-koeln.txt 	new file:   fc-st-pauli.json 	new file:   fc-st-pauli.txt 	new file:   holstein-kiel.json 	new file:   holstein-kiel.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -35,8 +35,8 @@ vereinslos=["Max Kruse", "Anwar El Ghazi"]
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1FC Heidenheim
 
-club="1 FC Köln"
-torneo="2023-24"
+club="FC St Pauli"
+torneo="2024-25"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
 klasspastclub="kick__vita__stationline-team"
