a8887c1c0482bc4797588af64baff1fa4a616243
enadol <enadol@yahoo.com>
2024-09-01 19:53:30 +0200

    modified:   bor-moenchengladbach.json 	modified:   bor-moenchengladbach.txt 	modified:   eintracht-frankfurt.json 	modified:   eintracht-frankfurt.txt 	modified:   fc-bayern-muenchen.json 	modified:   fc-bayern-muenchen.txt 	modified:   holstein-kiel.json 	modified:   holstein-kiel.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -40,7 +40,7 @@ no_games=["Exequiel Palacios", "Gustavo Puerta", "Woo-Yeong Jeong", "Gabriel Vid
 #Bayer 04 Leverkusen 1 FC Heidenheim 1 FC Union Berlin
 # 1 FSV Mainz 05
 
-club="Bayer 04 Leverkusen"
+club="Bor Mönchengladbach"
 torneo="2024-25"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
