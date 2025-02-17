8f5a3b6d47c9e7d748204d2a645ece699721ff89
enadol <enadol@yahoo.com>
2025-02-17 09:43:05 +0100

    modified:   1-fc-union-berlin.json 	modified:   1-fc-union-berlin.txt 	modified:   1-fsv-mainz-05.json 	modified:   1-fsv-mainz-05.txt 	modified:   bayer-04-leverkusen.json 	modified:   bayer-04-leverkusen.txt 	modified:   bor-moenchengladbach.json 	modified:   bor-moenchengladbach.txt 	modified:   borussia-dortmund.json 	modified:   borussia-dortmund.txt 	modified:   eintracht-frankfurt.json 	modified:   eintracht-frankfurt.txt 	modified:   fc-bayern-muenchen.json 	modified:   fc-bayern-muenchen.txt 	modified:   rb-leipzig.json 	modified:   rb-leipzig.txt 	modified:   scrpapteamwlptr24.py

--- scrpapteamwlptr24.py
+++ scrpapteamwlptr24.py
@@ -42,7 +42,7 @@ no_complete=["Matija Marsenic"]
 #Bayer 04 Leverkusen 1 FC Heidenheim 1 FC Union Berlin
 # 1 FSV Mainz 05 FC St Pauli VfL Bochum
 
-club="1 FC Union Berlin"
+club="Bor Mönchengladbach"
 torneo="2024-25"
 klassvita="kick__vita__header__person-detail-kvpair-info"
 klassfrom="kick__vita__header__team-detail__prime"
