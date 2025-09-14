578e37d06dc97c0ac7ebfd99d709d0dc4494b6d2
enadol <enadol@yahoo.com>
2025-09-14 11:07:21 +0200

    modified:   fc-bayern-muenchen.json 	modified:   fc-bayern-muenchen.txt 	modified:   teamSEL25-beta.py

--- teamSEL25-beta.py
+++ teamSEL25-beta.py
@@ -51,7 +51,7 @@ name_leave=["Fabio Vieira", "Arthur Chaves", "Luis Diaz", "Aleix Garcia", "Tiago
 #Bayer 04 Leverkusen 1 FC Heidenheim 1 FC Union Berlin
 # 1 FSV Mainz 05 FC St Pauli VfL Bochum Hamburger SV
 
-club="1 FC Union Berlin"
+club="FC Bayern München"
 torneo="2025-26"
 
 klassvita="kick__vita__header__person-detail-kvpair-info"
