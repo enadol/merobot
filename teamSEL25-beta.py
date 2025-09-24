f79f28ebfb7e82c5f3b52b73828820ec4de6c75e
enadol <enadol@yahoo.com>
2025-09-24 17:41:06 +0200

    modified:   1-fsv-mainz-05.json 	modified:   1-fsv-mainz-05.txt 	modified:   bayer-04-leverkusen.json 	modified:   bayer-04-leverkusen.txt 	modified:   teamSEL25-beta.py 	modified:   werder-bremen.json 	modified:   werder-bremen.txt

--- teamSEL25-beta.py
+++ teamSEL25-beta.py
@@ -52,7 +52,7 @@ name_leave=["Fabio Vieira", "Arthur Chaves", "Luis Diaz", "Aleix Garcia", "Tiago
 # 1 FC Köln
 # 1 FSV Mainz 05 FC St Pauli VfL Bochum Hamburger SV
 
-club="SC Freiburg"
+club="Bayer 04 Leverkusen"
 torneo="2025-26"
 
 klassvita="kick__vita__header__person-detail-kvpair-info"
