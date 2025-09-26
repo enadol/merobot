7961c05b631cf0de9574e0e1264d9c919590f2cf
enadol <enadol@yahoo.com>
2025-09-26 12:14:16 +0200

    modified:   teamSEL25-beta.py

--- teamSEL25-beta.py
+++ teamSEL25-beta.py
@@ -52,7 +52,7 @@ name_leave=["Fabio Vieira", "Arthur Chaves", "Luis Diaz", "Aleix Garcia", "Tiago
 # 1 FC Köln
 # 1 FSV Mainz 05 FC St Pauli VfL Bochum Hamburger SV
 
-club="Bayer 04 Leverkusen"
+club="Eintracht Frankfurt"
 torneo="2025-26"
 
 klassvita="kick__vita__header__person-detail-kvpair-info"
