73c9755754df72dcc5d32f896b15ef260b196455
enadol <enadol@yahoo.com>
2025-10-30 17:01:58 +0100

    modified:   bayer-04-leverkusen.json 	modified:   bayer-04-leverkusen.txt 	modified:   teamSEL25-beta.py

--- teamSEL25-beta.py
+++ teamSEL25-beta.py
@@ -49,10 +49,10 @@ name_leave=["Fabio Vieira", "Arthur Chaves", "Luis Diaz", "Aleix Garcia", "Tiago
 
 #Bor Mönchengladbach para Gladbach
 #Bayer 04 Leverkusen 1 FC Heidenheim 1 FC Union Berlin
-# 1 FC Köln
+# 1 FC Köln FC Bayern München
 # 1 FSV Mainz 05 FC St Pauli VfL Bochum Hamburger SV
 
-club="FC Bayern München"
+club="Bayer 04 Leverkusen"
 torneo="2025-26"
 
 klassvita="kick__vita__header__person-detail-kvpair-info"
